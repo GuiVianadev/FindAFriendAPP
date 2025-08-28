879b5befee4609c62c32d08287ed00edd8c5f7be
GuiVianaDev <guilhermevianna@gmail.com>
2025-08-28 11:04:41 -0300

    feat: initial backend config

---
+++ backend/src/env/index.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod';
+
+const DEFAULT_PORT = 3333;
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  PORT: z.coerce.number().default(DEFAULT_PORT),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (_env.success === false) {
+  console.error(z.treeifyError(_env.error));
+
+  throw new Error('Invalid enviroment variables.');
+}
+
+export const env = _env.data;
